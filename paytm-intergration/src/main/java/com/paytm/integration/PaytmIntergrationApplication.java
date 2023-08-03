34112bb90fdfc98ceb35bba2201cc700d642134d
Aman Haldiya <Amanhaldiya512@gmail.com>
2023-08-03 23:23:36 +0530

    first commit

---
+++ paytm-intergration/src/main/java/com/paytm/integration/PaytmIntergrationApplication.java
@@ -0,0 +1,13 @@
+package com.paytm.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytmIntergrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytmIntergrationApplication.class, args);
+	}
+
+}
